189355e2187d9e3d32f13419b7a24090ee443d41
Sana Afreen <s.afreen42789@GMAIL.COM>
2018-06-24 11:46:34 -0500

    packages

---
+++ server.js
@@ -0,0 +1,23 @@
+var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+var express = require('express');
+var ehandlebar = require('express-handlebars');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var request = require('request');
+
+//init express
+var app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+//Serve Static 
+app.use(express.static(process.cwd() + '/public'));
+
+//ehandlebars
+app.engine('handlebars', ehandlebar({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//data config to mongoose
