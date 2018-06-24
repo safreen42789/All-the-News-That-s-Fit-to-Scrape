a378dfce77edac3d4ad51715f30be6f5a03abfdb
Sana Afreen <s.afreen42789@GMAIL.COM>
2018-06-24 18:33:36 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # All-the-News-That-s-Fit-to-Scrape
+Welcome to All-the-News-That-s-Fit-to-Scrape! Here we have a web app that lets users leave comments on the latest news. But you're not going to actually write any articles; instead, we flex your Mongoose and Cheerio muscles to scrape news from another site.  Happy reading!
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
     extended: false
-}))
+}));
 
 //Serve Static 
 app.use(express.static(process.cwd() + '/public'));
@@ -22,7 +22,16 @@ app.engine('handlebars', ehandlebar({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //data config to mongoose
-mongoose.connect('mongodb://localhost:27017');
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+//mongoose.connect('mongodb://localhost:27017');
 var db = mongoose.connection;
 // Show any mongoose errors
 db.on("error", function(error) {
@@ -31,7 +40,7 @@ db.on("error", function(error) {
   
 // Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
-console.log("Mongoose connection successful.");
+console.log("Mongoose connection was successful.");
   });
 
 //comment and article models
